bb1dd31a815b1fcf7c25ffa5d935d4e718f5ea00
DevAlissu <alison.silva8741@gmail.com>
2025-07-11 13:28:36 -0400

    prototipo v5

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path";
-import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,8 +15,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
+      "@": "/src",
+      "@styles": "/src/styles",
     },
   },
 });
