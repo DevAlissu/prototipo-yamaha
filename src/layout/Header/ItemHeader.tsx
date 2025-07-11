66452bc101a582db8b4aa3c91802eac59ca4d09d
DevAlissu <alison.silva8741@gmail.com>
2025-07-11 14:06:35 -0400

    prototipo v13

--- src/layout/Header/ItemHeader.tsx
+++ src/layout/Header/ItemHeader.tsx
@@ -30,7 +30,7 @@ export default function ItemHeader() {
           alignItems: "center",
           justifyContent: "center",
           flex: 1,
-          marginLeft: "-24px",
+          marginLeft: "-26px",
         }}
       >
         <img
